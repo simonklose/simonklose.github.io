ff92068edb88a8e87720d194eb606e0baca6f14e
SimonKlose <30284294+simonklose@users.noreply.github.com>
2019-03-24 18:14:24 +0800

    Update love.js

--- js/love.js
+++ js/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("2018年03月14日", 23, 10);
+            ctx.fillText("3月14日", 23, 10);
             ctx.restore();
         },
         clear: function() {
